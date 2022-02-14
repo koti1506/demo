5a13ffa70efc21f74c3963e9bae62f19b122c6df
koti <kotid2021@gmail.com>
2022-02-14 07:00:38 +0530

    this is first commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/msg")
+	public String getMsg(@RequestParam Optional<String> name) {
+		return "Name :"+name.orElseGet(()->"Name is not Provided");
+		
+	}
+
+}
